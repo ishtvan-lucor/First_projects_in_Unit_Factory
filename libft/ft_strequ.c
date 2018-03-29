95f5985727b93599195c14975bf72dac0180a3b7
Ishtvan KOLOSHYNSKY <ikoloshy@e2r2p3.unit.ua>
2018-03-29 21:05:37 +0300

    added: strequ, strnequ

---
+++ libft/ft_strequ.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/29 18:27:55 by ikoloshy          #+#    #+#             */
+/*   Updated: 2018/03/29 19:56:47 by ikoloshy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strequ(char const *s1, char const *s2)
+{
+	size_t	index;
+
+	index = -1;
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	while(s1[++index] != '\0' && s2[index] != '\0')
+		if (s1[index] != s2[index])
+			return (0);
+	if (s1[index] != s2[index])
+		return (0);
+	return (1);
+}
