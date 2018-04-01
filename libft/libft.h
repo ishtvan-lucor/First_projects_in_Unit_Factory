a7dce3337ccccecfb66cc1d95f637310e8a4de94
Ishtvan KOLOSHYNSKY <ikoloshy@e2r1p1.unit.ua>
2018-04-01 22:21:59 +0300

    added 5 func

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 17:27:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/03/31 22:30:20 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2018/04/01 22:20:48 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	ft_putstr(char const *s);
 void	ft_strclr(char *s);
 void	ft_strdel(char **as);
 void	*ft_memalloc(size_t size);
-void	*ft_memcpy(void *dst, void const *src, size_t n);
-void	*ft_memccpy(void *dst, void const *src, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 char 	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
@@ -34,8 +34,13 @@ char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strnew(size_t size);
-int		ft_strequ(char const *s1, char const *s2);
-int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strequ(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strnequ(const char*s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 
 #endif
