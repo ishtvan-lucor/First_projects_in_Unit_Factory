ea69f1fa2110b81c7086c05a87a1174038ec8453
Ishtvan KOLOSHYNSKY <ikoloshy@e2r1p5.unit.ua>
2018-04-02 20:53:13 +0300

    added new function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 17:27:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/04/01 22:20:48 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2018/04/02 20:25:40 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,25 @@ void	*ft_memset(void *b, int c, size_t len);
 char 	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *s1);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strnew(size_t size);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strequ(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strnequ(const char*s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 
 #endif
