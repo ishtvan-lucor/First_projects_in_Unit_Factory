cdcde5d6f83667eb93afb321847f95fb33ecbeee
Ishtvan KOLOSHYNSKY <ikoloshy@e3r1p9.unit.ua>
2018-04-03 21:36:29 +0300

    added new funct

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 17:27:31 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/04/02 20:25:40 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2018/04/03 21:31:08 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,15 @@ void	ft_bzero(void *s, size_t n);
 
 void	ft_memdel(void **ap);
 void	ft_putchar(char c);
+void	ft_putendl(char const *s);
 void	ft_putstr(char const *s);
 void	ft_strclr(char *s);
 void	ft_strdel(char **as);
 void	*ft_memalloc(size_t size);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char 	*ft_strcat(char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
@@ -38,18 +41,21 @@ char	*ft_strnew(size_t size);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strequ(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strnequ(const char*s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 
 #endif
