c07fca0832801fdb10d239a8ab61b96411cd4e58
Ishtvan KOLOSHYNSKY <ikoloshy@e2r1p1.unit.ua>
2018-04-04 21:04:54 +0300

    finished

--- libft/ft_strcpy.c
+++ libft/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 22:42:25 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/03/30 23:06:15 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2018/04/04 17:47:59 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strcpy(char *dst, const char *src)
 	size_t	index;
 
 	index = 0;
-	while (dst[index] != '\0' && src[index] != '\0')
+	while (src[index] != '\0')
 	{
 		dst[index] = src[index];
 		index++;
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/29 20:10:42 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/04/04 16:59:01 by ikoloshy         ###   ########.fr       */
+/*   Created: 2018/04/04 18:46:41 by ikoloshy          #+#    #+#             */
+/*   Updated: 2018/04/04 18:50:17 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ss;
-	
-	if (s == NULL)
+	char	*bigstr;
+
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	if ((ss = ft_strnew(len)) == NULL)
-		return (ss);
-	ss = ft_strncpy(ss, (s + start), len);
-	return (ss);
+	if ((bigstr = ft_strnew(ft_strlen(s1) + ft_strlen(s2))) == NULL)
+		return (bigstr);
+	ft_strcpy(bigstr, s1);
+	ft_strcat(bigstr, s2);
+	return (bigstr);
 }
