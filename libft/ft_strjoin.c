a7f6ff5dd0001fc72216847b23dd5d409a37baf8
Ishtvan KOLOSHYNSKY <ikoloshy@e2r1p4.unit.ua>
2018-04-18 21:19:00 +0300

    finished

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ikoloshy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 18:46:41 by ikoloshy          #+#    #+#             */
-/*   Updated: 2018/04/04 18:50:17 by ikoloshy         ###   ########.fr       */
+/*   Updated: 2018/04/16 20:28:32 by ikoloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*bigstr;
+	char	*str1;
+	char	*str2;
 
-	if (s1 == NULL || s2 == NULL)
+	str1 = (char*)s1;
+	str2 = (char*)s2;
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		return (str2);
+	if (s2 == NULL)
+		return (str1);
 	if ((bigstr = ft_strnew(ft_strlen(s1) + ft_strlen(s2))) == NULL)
 		return (bigstr);
 	ft_strcpy(bigstr, s1);
